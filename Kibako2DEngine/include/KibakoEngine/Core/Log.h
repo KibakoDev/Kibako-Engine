e1b08ab4856e5fb531812f632492e94490a57be9
Kibako Dev <kibakodev@gmail.com>
2025-11-12 23:39:02 -0500

    Simplify logging macros for MSVC compatibility

--- Kibako2DEngine/include/KibakoEngine/Core/Log.h
+++ Kibako2DEngine/include/KibakoEngine/Core/Log.h
@@ -19,7 +19,10 @@ namespace KibakoEngine {
 
 #define KBK_LOG_CHANNEL_DEFAULT "Kibako"
 
-#define KbkLog(channel, fmt, ...)   ::KibakoEngine::LogMessage(::KibakoEngine::LogLevel::Info, (channel), (fmt) __VA_OPT__(, ) __VA_ARGS__)
-#define KbkWarn(channel, fmt, ...)  ::KibakoEngine::LogMessage(::KibakoEngine::LogLevel::Warning, (channel), (fmt) __VA_OPT__(, ) __VA_ARGS__)
-#define KbkError(channel, fmt, ...) ::KibakoEngine::LogMessage(::KibakoEngine::LogLevel::Error, (channel), (fmt) __VA_OPT__(, ) __VA_ARGS__)
+#define KbkLog(...) \
+    ::KibakoEngine::LogMessage(::KibakoEngine::LogLevel::Info, __VA_ARGS__)
+#define KbkWarn(...) \
+    ::KibakoEngine::LogMessage(::KibakoEngine::LogLevel::Warning, __VA_ARGS__)
+#define KbkError(...) \
+    ::KibakoEngine::LogMessage(::KibakoEngine::LogLevel::Error, __VA_ARGS__)
 
